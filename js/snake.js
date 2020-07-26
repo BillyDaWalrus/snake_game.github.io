b59413707cd23f454ff923c054ee06fa4d9df54b
BillyDaWalrus <bill6399@gmail.com>
2020-07-26 16:44:30 -0400

    added player and movement

--- js/snake.js
+++ js/snake.js
@@ -1,24 +1,89 @@
-import {Food} from "./food";
+const GAME_SPEED = 30
+const GAME_WIDTH = 1000
+const GAME_HEIGHT = 500
 
-var Snake_Head = function () {
-    var ctx = $('#Canvas')[0].getContext('2d');
-    ctx.beginPath();
-    ctx.fill(255, 0, 0)
-    ctx.rect(20, 20, 100, 100);
-    ctx.stroke();
+class Snake_Head {
+    snake_length = 2
+    snake = []
+    x = GAME_WIDTH/2
+    y = GAME_HEIGHT/2
+    speed = 5
+    direction = null
+    canvas = $('#Canvas')[0];
+    ctx = this.canvas.getContext('2d');
+
+    constructor() {
+        this.ctx.canvas.width = GAME_WIDTH
+        this.ctx.canvas.height = GAME_HEIGHT
+    }
+
+    //Draws the Snake
+    snake_draw() {
+        this.snake.unshift([this.x, this.y])
+        for (var i = 0; i < this.snake.length; i++) {
+            //Resets Canvas
+            this.ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+
+            //Draws Snake
+            for (var i = 0; i < this.snake.length; i++) {
+                this.ctx.fillRect(this.snake[i][0], this.snake[i][1], 25, 25);
+                this.snake.pop();
+            }
+        }
+    }
+
+    //Movement
+    movement() {
+        if (this.direction === 'up') {
+            this.y -= this.speed;
+        }
+
+        if (this.direction === 'down') {
+            this.y += this.speed;
+        }
+
+        if (this.direction === 'left') {
+            this.x -= this.speed;
+        }
+
+        if (this.direction === 'right') {
+            this.x += this.speed;
+        }
+    }
+
+    //Check Direction Change
+    direction_change(new_direction) {
+        this.direction = new_direction;
+    }
 }
 
-class Snake{
-    constructor(x, y) {
-        this.x = x
-        this.y = y
-        this.food = new Food(50, 50)
-    }
-    print() {
-        console.log(this.x, this.y)
-    }
-}
 
-var player = new Snake(50, 60)
-player.print()
+var player = new Snake_Head()
+setInterval(() => {
+    //Check Direction
+    $(document).keydown(function (event) {
+        //Player Movement: Up
+        if (event.which === 87) {
+            player.direction_change('up');
+        }
 
+        //Player Movement: Down
+        if (event.which === 83) {
+            player.direction_change('down');
+        }
+
+        //Player Movement: Left
+        if (event.which === 65) {
+            player.direction_change('left');
+        }
+
+        //Player Movement: Right
+        if (event.which === 68) {
+            player.direction_change('right');
+        }
+    });
+    
+    player.movement();
+    player.snake_draw();
+
+}, GAME_SPEED);
